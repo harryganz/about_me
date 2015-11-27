82c91a6a0420c9bc7041e6300beea434d0c27acf
Harry Ganz <hganz@rsmas.miami.edu>
2015-11-27 15:08:56 -0500

    Adds page header

--- js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $('#nav li a').on('click', function(){
-    $('#nav li a').removeClass('active');
-    $(this).addClass('active');
-    var address = $(this).attr("href");
-    $('.panel').removeClass('active');
-    $(address).addClass('active')
-  })
-})
